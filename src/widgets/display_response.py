4abfac0e45258e7b9e1acddaed5dc9449852aa56
Zero-Security <contact@zero-security.net>
2025-03-11 18:07:38 +0100

    refacor: changed tree structure, added absolut paths and catch unknown commands and do not send them to the CoreMiner

--- src/widgets/display_response.py
+++ src/widgets/display_response.py
@@ -3,4 +3,5 @@ from textual.widgets import Static
 class ResponseDisplay(Static):
     """Displays responses from CoreMiner"""
     def update_content(self, message: str):
-        self.update(f"Response: {message}")
+        self.update(f"Response: {message} ")
+    # Arguments do not get displayed at the moment
